d9724b26cd90158663942283f19852d9b9a30ecf
KingVE00 <kwe4thoct@gmail.com>
2023-08-21 18:44:44 -0700

    Printf Test Updates

--- _printf.c
+++ _printf.c
@@ -1,51 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
-/**
- * _printf - functions the same as printf function
- * @format: The first fixed argument
- * @...: variable arguments
- * Return: number of character printed
- */
+
 int _printf(const char *format, ...)
 {
-	int size, j, k;
-	va_list arg_list;
-	char *my_str;
-	char tmp;
+    va_list args;
+    va_start(args, format);
 
-	k = 0;
-	va_start(arg_list, format);
-	for (size = 0; format[size]; size++)
-	{
-	}
-	my_str = malloc(sizeof(format) * size);
-	for (j = 0; j < size; j++)
-	{
-		my_str[j] = format[j];
-	}
-	for (j = 0; j < size; j++)
-	{
-		k = j;
-		if (my_str[j] == '%')
-		{
-			if (my_str[++k] == 'c')
-			{
-				my_str[j] = va_arg(arg_list, int);
-				my_str[k] = 3;
-			}
-			if (my_str[k] == '%')
-			{
-				my_str[++j] = 3;
-			}
-			if (my_str[k] == 'd' || my_str[k] == 'i')
-			{
-				tmp = va_arg(arg_list, int);
-				my_str[j] = (tmp / 10) + '0';
-				my_str[k] = (tmp % 10) + '0';
-			}
-		}
+    int cnt = 0;
+    for (int i = 0; format[i]; i++) {
+        if (format[i] == '%') {
+            if (format[i + 1] != '\0') {
+                int (*function)(va_list) = decision(format[i + 1]);
+                if (function != NULL) {
+                    cnt += function(args);
+                    i++;
+                } else {
+                    _putchar(format[i]);
+                    cnt++;
+                }
+            }
+        } else {
+            _putchar(format[i]);
+            cnt++;
+        }
+    }
 
-	}
-	printf("%s", my_str);
-	return (size);
+    va_end(args);
+    return cnt;
 }
